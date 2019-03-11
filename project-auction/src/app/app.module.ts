ed70e28d941a786b9579dc77d3599655d3e8950a
Lucas Dos Santos Celestino <lucas.celestino@sp01.local>
2019-03-11 12:01:00 -0300

    literais

--- project-auction/src/app/app.module.ts
+++ project-auction/src/app/app.module.ts
@@ -1,10 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
+import { ThfI18nModule, ThfI18nConfig } from '@totvs/thf-ui/services/thf-i18n';
 import { ThfModule } from '@totvs/thf-ui';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { generalEn } from './i18n/general-en';
+import { generalPt } from './i18n/general-pt';
+
+const i18nConfig: ThfI18nConfig = {
+  default: {
+    language: 'pt-BR',
+    context: 'general',
+    cache: true
+  },
+  contexts: {
+    general: {
+      'pt-BR': generalPt,
+      'en-US': generalEn
+    },
+  }
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +31,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ThfModule
+    ThfModule,
+    ThfI18nModule.config(i18nConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
